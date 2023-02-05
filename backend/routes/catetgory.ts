f1658b8bfa151a026ac930b45139f86abea9efe9
Aung KO <aungko.uit@gmail.com>
2023-02-05 07:15:08 +0630

    refactor: Using async, await instead of callback

--- backend/routes/catetgory.ts
+++ backend/routes/catetgory.ts
@@ -1,97 +1,81 @@
 import Category from '../models/category'
-import {Router, Request, Response} from 'express'
-import {param, validationResult} from 'express-validator'
+import { Router, Request, Response } from 'express'
+import { param, validationResult } from 'express-validator'
 const router = Router()
 
+router.get('/categories', async (_req: Request, res: Response) => {
+  try {
+    const data = await Category.find({})
+    return res.status(200).json({ data })
+  } catch (err) {
+    console.error(err)
+    return res.sendStatus(500)
+  }
+})
 
-router.get('/categories', (_req: Request, res: Response) => {
-    try {
-      Category.find({}, (err, data: object) => {
-        if (err) {
-          console.error(err)
-          res.sendStatus(500)
-        } else {
-          res.status(200).json({data})
-        }
-      })
+router.post('/categories', async (req: Request, res: Response) => {
+  const category = {
+    categoryName: req.body.productName
+  }
+
+  await Category.create(category, (err, data) => {
+    if (err) {
+      return res.sendStatus(500)
     }
-    catch(err) {
-      res.sendStatus(500)
-    }
+    const id = data.id
+    res.append('Location', 'http://localhost:8000/categories' + id)
+    return res.status(201).json({ meta: { id }, data })
   })
-  
-  router.post('/categories',async (req: Request, res: Response) => {
-    const category = {
-      categoryName: req.body.productName,
-    }
-  
-      await Category.create(category, (err, data) => {
-        if (err) {
-          return res.sendStatus(500)
-        }
-        const id = data.id
-        res.append('Location', 'http://localhost:8000/categories' + id)
-        return res.status(201).json({meta: {id}, data})
-      })
-  })
-  
-  router.put('/categories/:id', [
-    param('id').isMongoId()
-  ], (req: Request, res: Response) => {
+})
+
+router.put(
+  '/categories/:id',
+  [param('id').isMongoId()],
+  async (req: Request, res: Response) => {
     const id = req.params.id
-  
+
     const errors = validationResult(req)
-    if(!errors.isEmpty()) {
-      return res.status(400).json({errors: errors.array()})
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
     }
-    Category.count({
-      _id: id
-    }, (err, count) => {
-      if (err) {
-        console.error(err)
-        return res.sendStatus(400)
-      }
+    try {
+      const count = await Category.count({
+        _id: id
+      })
       const updatedCategory = {
         _id: id,
         ...req.body
       }
       console.log(updatedCategory)
       if (count) {
-        Category.findByIdAndUpdate(id, updatedCategory, (err, data) => {
-          if (err) {
-            console.error(err)
-            return res.sendStatus(500)
-          } else {
-            return res.status(200).json({
-                meta: {id},
-                data
-            })
-          }
-        })
-      } 
-    })
-  })
-  
-  router.delete('/categories/:id', (req, res) => {
-    const id = req.params.id
-    Category.count({_id: id}, (err, count) => {
-      if (err) {
-        console.error(err)
-        return res.sendStatus(404)
-      }
-      if(count) {
-        Category.findByIdAndRemove(id, (err) => {
-          if (err) {
-            console.error(err)
-            return res.sendStatus(500)
-          } else {
-            return res.sendStatus(204)
-          }
-        } )
-      } else {
-        return res.sendStatus(404)
-      }
-    })
-  })
+        const data = await Category.findByIdAndUpdate(id, updatedCategory)
 
-  export default router
+        return res.status(200).json({
+          meta: { id },
+          data
+        })
+      }
+    } catch (err) {
+      return res.sendStatus(400)
+    }
+  }
+)
+
+router.delete('/categories/:id', async (req, res) => {
+  const id = req.params.id
+  const count = await Category.count({ _id: id })
+
+  if (count) {
+    try {
+      await Category.findByIdAndRemove(id)
+      return res.sendStatus(204)
+    } catch (err) {
+      console.error(err)
+      return res.sendStatus(500)
+    }
+  } else {
+    return res.sendStatus(404)
+  }
+})
+
+export default router
--- backend/routes/products.ts
+++ backend/routes/products.ts
@@ -1,108 +1,96 @@
-import {Router, Request, Response} from 'express'
+import { Router, Request, Response } from 'express'
 import { body, param, validationResult } from 'express-validator'
 import Product from '../models/product'
-    
 
 const router = Router()
 
-router.get('/products', (_req: Request, res: Response) => {
-    try {
-      Product.find({}, (err, data: object) => {
-        if (err) {
-          console.error(err)
-          res.sendStatus(500)
-        } else {
-          res.status(200).json({data})
-        }
-      })
-    }
-    catch(err) {
-      res.sendStatus(500)
-    }
-  })
-  
-  router.post('/products', [
-    body('productName').not().isEmpty(),
-    body('price').not().isEmpty()
-  ],async (req: Request, res: Response) => {
+router.get('/products', async (_req: Request, res: Response) => {
+  try {
+    const data = await Product.find({})
 
+    return res.status(200).json({ data })
+  } catch (err) {
+    console.error(err)
+    return res.sendStatus(500)
+  }
+})
+
+router.post(
+  '/products',
+  [body('productName').not().isEmpty(), body('price').not().isEmpty()],
+  async (req: Request, res: Response) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
-      return res.status(400).json({errors: errors.array()})
+      return res.status(400).json({ errors: errors.array() })
     }
     const product = {
       productName: req.body.productName,
       price: req.body.price,
-      stock: req.body.stock,
+      stock: req.body.stock
     }
-  
-      await Product.create(product, (err, data) => {
-        if (err) {
-          return res.sendStatus(500)
-        }
-        const id = data.id
-        res.append('Location', 'http://localhost:8000/products' + id)
-        return res.status(201).json({meta: {id}, data})
-      })
-  })
-  
-  router.put('/products/:id', [
-    param('id').isMongoId()
-  ], (req: Request, res: Response) => {
+
+    try {
+      const data = await Product.create(product)
+      const id = data.id
+      res.append('Location', 'http://localhost:8000/products' + id)
+      return res.status(201).json({ meta: { id }, data })
+    } catch (err) {
+      return res.sendStatus(500)
+    }
+  }
+)
+
+router.put(
+  '/products/:id',
+  [param('id').isMongoId()],
+  async (req: Request, res: Response) => {
     const id = req.params.id
-  
+
     const errors = validationResult(req)
-    if(!errors.isEmpty()) {
-      return res.status(400).json({errors: errors.array()})
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
     }
-    Product.count({
-      _id: id
-    }, (err, count) => {
-      if (err) {
-        console.error(err)
-        return res.sendStatus(400)
-      }
+
+    try {
+      const count = await Product.count({
+        _id: id
+      })
       const updatedProduct = {
         _id: id,
         ...req.body
       }
       console.log(updatedProduct)
-      if (count) {
-        Product.findByIdAndUpdate(id, updatedProduct, (err, data) => {
-          if (err) {
-            console.error(err)
-            return res.sendStatus(500)
-          } else {
-            return res.status(200).json({
-                meta: {id},
-                data
-            })
-          }
-        })
-      } 
-    })
-  })
-  
-  router.delete('/products/:id', (req, res) => {
-    const id = req.params.id
-    Product.count({_id: id}, (err, count) => {
-      if (err) {
-        console.error(err)
-        return res.sendStatus(404)
-      }
-      if(count) {
-        Product.findByIdAndRemove(id, (err) => {
-          if (err) {
-            console.error(err)
-            return res.sendStatus(500)
-          } else {
-            return res.sendStatus(204)
-          }
-        } )
-      } else {
-        return res.sendStatus(404)
-      }
-    })
-  })
 
-  export default router
+      if (count) {
+        const data = await Product.findByIdAndUpdate(id, updatedProduct, {
+          new: true
+        })
+        return res.status(200).json({
+          meta: { id },
+          data
+        })
+      }
+    } catch (err) {
+      console.error(err)
+      return res.sendStatus(400)
+    }
+  }
+)
+
+router.delete('/products/:id', async (req, res) => {
+  const id = req.params.id
+  const count = await Product.count({ _id: id })
+  if (count) {
+    try {
+      await Product.findByIdAndRemove(id)
+      return res.sendStatus(204)
+    } catch (err) {
+      console.error(err)
+      return res.sendStatus(500)
+    }
+  } else {
+    return res.sendStatus(404)
+  }
+})
+
+export default router
